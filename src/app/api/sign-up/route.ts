d3bca8f1db77dc78613f2cce1863f065a85c9ea8
Arqam Habib Ghauri <arqamghauri3@gmail.com>
2025-05-24 20:39:08 +0500

    Implement user authentication with email verification and session management using next-auth

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,11 +3,81 @@ import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
+
 export async function POST(request: Request) {
   await dbConnect();
 
   try {
     const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json({
+          success: false,
+          message: "User already registered.",
+        });
+      } else {
+        existingUserByEmail.verifyCode = verifyCode
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+        await existingUserByEmail.save()
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        password: hashedPassword,
+        email,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+      });
+      await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User Registered Successfully. Please Verify your email.",
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error Registering user", error);
     return Response.json(
